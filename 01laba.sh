42e6fe18a93402bf6d8b6d61e2f31f17f0c6564e
root <root@localhost.localdomain>
2020-09-21 17:37:05 +0300

    Добавление основных файлов

---
+++ 01laba.sh
@@ -0,0 +1,16 @@
+#!bin/bash
+exec 0<>/dev/console 1<>/dev/console 2<>/dev/console cat <<'msgend'
+
+<Nikel's dracut module>
+
+--------------------------------
+|   N   N  N  N            N   |
+|   NN  N     N  N   NNN   N   |
+|   N N N  N  N N   NNNNN  N   |
+|   N  NN  N  NN    N      N   |
+|   N   N  N  N NN   NNN   N   |
+-------------------------------
+
+msgend
+sleep 1000
+echo " continuing....
